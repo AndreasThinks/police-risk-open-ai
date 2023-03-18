4ac5c3c670e6507291b12f60f2e03f7f25651faa
Andreas Varotsis <andreas.varotsis@gmail.com>
2023-03-18 10:47:47 +0000

    added basic functions to query open AI API

--- police_risk_open_ai/_modidx.py
+++ police_risk_open_ai/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://AndreasThinks.github.io',
                 'git_url': 'https://github.com/AndreasThinks/police-risk-open-ai',
                 'lib_path': 'police_risk_open_ai'},
-  'syms': {'police_risk_open_ai.core': {'police_risk_open_ai.core.foo': ('core.html#foo', 'police_risk_open_ai/core.py')}}}
+  'syms': {'police_risk_open_ai.core': {'police_risk_open_ai.core.query_llm': ('llm.html#query_llm', 'police_risk_open_ai/core.py')}}}
